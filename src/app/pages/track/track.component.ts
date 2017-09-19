1a76528a12143371b9bfb6a6e9c7ca08ff1ace29
suiyeqian <m.qian@foxmail.com>
2017-09-19 13:35:52 +0800

    modified for percent over 100

--- src/app/pages/track/track.component.ts
+++ src/app/pages/track/track.component.ts
@@ -56,7 +56,7 @@ export class TrackComponent implements OnInit, AfterContentInit {
           if ( res.code === 0) {
             this.achievement = res.data;
             echart.ProgressChartOptions.series[0].data[0].value = res.data.monSaleRate;
-            echart.ProgressChartOptions.series[0].data[1].value = 100 - res.data.monSaleRate;
+            echart.ProgressChartOptions.series[0].data[1].value = (100 - res.data.monSaleRate < 0) ? 0 : (100 - res.data.monSaleRate);
             this.saleProgressOption = echart.ProgressChartOptions;
           }
         });
